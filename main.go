334e73f82965d39e0aa5421ca981c24c4151ec01
wenqiang <wenqiangsspu@163.com>
2024-06-20 12:21:41 +0800

    update: ignore

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 
 	// 获取今天的日期，格式为GitHub API使用的日期（例如：2023-06-12）
 	today := time.Now().UTC().Format("2006-01-02")
-	taskPool := utils.GenerateTaskPool("participant-test.json")
+	taskPool := utils.GenerateTaskPool("participant.json")
 	service.Checkout(taskPool, today)
 }
